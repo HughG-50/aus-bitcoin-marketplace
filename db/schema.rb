a1a079243f2f8cb9e7c12a02052b33d57f78373a
HughG-50 <hugh.greethead@gmail.com>
2020-05-13 16:39:42 +1000

    Listing model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_061037) do
+ActiveRecord::Schema.define(version: 2020_05_13_063651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2020_05_13_061037) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "payment_method"
+    t.integer "price_BTC_AUD"
+    t.integer "amount"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +69,5 @@ ActiveRecord::Schema.define(version: 2020_05_13_061037) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "listings", "users"
 end
