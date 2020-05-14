2116ef0be470811cb44f12b42073b211a060bc30
HughG-50 <hugh.greethead@gmail.com>
2020-05-14 15:44:27 +1000

    Show profile basic view and controller implemented

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,18 +1,19 @@
 class ListingsController < ApplicationController
   # Testing that devise Gem worked
-  before_action :authenticate_user!
+  # before_action authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index]
+
   def index
     @listings = Listing.all
-    
+  end
+
+  def show
   end
 
   def new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # listings routes
   resources :listings
 
+  # homepage route
   get "/", to: "pages#home", as: "root"
+
+  # profile page routes
+  get "/profile/:id", to: "profiles#show", as: "user"
+  put "/profile/:id", to: "profiles#edit"
+  patch "/profile/:id", to: "profiles#edit"
+  post "/profile/:id", to: "profiles#update"
+  get "/profile/:id/edit", to: "profiles#edit", as: "edit_profile"
 end
