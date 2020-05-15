1cb27e29c28a3d4d22a4b70cc4d7f238f1cdc1eb
HughG-50 <hugh.greethead@gmail.com>
2020-05-15 17:16:43 +1000

    Edit, update and create actions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,19 +14,43 @@ class ListingsController < ApplicationController
   # By default this is the listing we will load when a user clicks on a listing
   # must be logged in
   def show
-    @listing = Listing.find(params[:id])
+    set_listing
   end
 
   def new
+    @listing = Listing.new
+    # Hardcoding that payments must be CC until functionality to other payments is expanded
+    # NEED TO CHECK WHETHER THIS IS VALID TO DO LIKE THIS
+    
+    # Also check User ID whether it needs to be in the input
+    @listing.payment_method = "Credit Card"
+    # @listing.save!
   end
 
   def create
+    @listing = current_user.listings.create(listing_params)
+
+    if @listing.errors.any?
+        render "new"
+    else 
+        redirect_to listings_path
+    end
+
   end
 
   def edit
+    @listing = Listing.new
+    @listing.payment_method = "Credit Card"
   end
 
   def update
+    # instead of create we are updating
+    @listing = Listing.update(params["id"], listing_params)
+    if @listing.errors.any?
+        render "edit"
+    else 
+        redirect_to listings_path
+    end
   end
 
   # TO DO // Consider changing redirect to dashboard once its been implemented
@@ -35,4 +59,15 @@ class ListingsController < ApplicationController
     
     redirect_to listings_path
   end
+
+  private 
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :payment_method, :price_BTC_AUD, :amount, :user_id)
+  end
+
 end
