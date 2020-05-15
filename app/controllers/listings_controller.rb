cbe70ec084f82a5b7b91b9127577888d0b3e7a3c
HughG-50 <hugh.greethead@gmail.com>
2020-05-15 14:22:30 +1000

    Show and delete listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,20 @@
 class ListingsController < ApplicationController
   # Testing that devise Gem worked
   # before_action authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:index]
+  before_action :authenticate_user!, :except => [:index, :show_basic]
 
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order(price_BTC_AUD: :asc)
   end
 
+  def show_basic
+
+  end
+
+  # By default this is the listing we will load when a user clicks on a listing
+  # must be logged in
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def new
@@ -22,6 +29,10 @@ class ListingsController < ApplicationController
   def update
   end
 
+  # TO DO // Consider changing redirect to dashboard once its been implemented
   def destroy
+    Listing.find(params["id"]).destroy
+    
+    redirect_to listings_path
   end
 end
