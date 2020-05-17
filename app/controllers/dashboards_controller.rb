5fdedfaa168982502a29043a7ad518e6308ec77c
HughG-50 <hugh.greethead@gmail.com>
2020-05-17 22:44:13 +1000

    ORM debug and display

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,9 @@
 class DashboardsController < ApplicationController
-    
+    before_action :authenticate_user!
+
     # Seller dashboard
     def pending_listings_index
+        set_user_listing
 
     end
 
@@ -21,4 +23,29 @@ class DashboardsController < ApplicationController
     def purchase_history_index
 
     end
+
+
+    private
+
+    def set_user_listing
+        # only logged in user accesses Dashboard pages
+        @user = current_user
+        @listings = current_user.listings
+
+        # Listings that have had purchase made - i.e. a listing that has a Purchase object
+        # @listings_purchased = current_user.listings.includes(:purchase)
+
+        # NEED TO FIND COMMANDS FOR RETURNING LIST OF LISTINGS THAT HAVE PURCHASE OBJECTS (I.E. BEEN PURCHASED)
+        # COMMAND FOR RETURNING LIST OF PURCHASES ASSOCIATED WITH LISTINGS LIST
+
+        # boolean check
+        # Purchase.where(listing_id: 1).empty?
+        # Or
+        # Purchase.exists?(:purchase_id)
+        # 
+        # Does purchase exist where BTC is sent?
+        # Purchase.exists?(:conditions => "BTC_sent = true")
+     end
+
+
 end
