7219fe8c56393079f759f74a5c69b952a20ff972
HughG-50 <hugh.greethead@gmail.com>
2020-05-17 18:28:08 +1000

    dashboard controller and views init

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,24 @@
+class DashboardsController < ApplicationController
+    
+    # Seller dashboard
+    def pending_listings_index
+
+    end
+
+    def current_listings_index
+
+    end
+
+    def completed_listings_index
+
+    end
+
+    # Buyer dashboard
+    def current_purchase_orders_index
+
+    end
+
+    def purchase_history_index
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,11 @@ Rails.application.routes.draw do
   patch "/profile/:id", to: "profiles#edit"
   post "/profile/:id", to: "profiles#update"
   get "/profile/:id/edit", to: "profiles#edit", as: "edit_profile"
+
+  # Dashboard routes
+  get "/dashboard/:id/pending_listings", to: "dashboards#pending_listings_index", as: "pending_listings"
+  get "/dashboard/:id/current_listings", to: "dashboards#current_listings_index", as: "current_listings"
+  get "/dashboard/:id/completed_listings", to: "dashboards#completed_listings_index", as: "completed_listings"
+  get "/dashboard/:id/current_purchase_orders", to: "dashboards#current_purchase_orders_index", as: "current_purchase_orders"
+  get "/dashboard/:id/purchase_history", to: "dashboards#purchase_history_index", as: "purchase_history"
 end
