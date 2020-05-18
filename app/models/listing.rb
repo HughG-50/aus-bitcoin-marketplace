31496b2180d6c0204798f259f764f11c552a2837
HughG-50 <hugh.greethead@gmail.com>
2020-05-18 10:57:39 +1000

    Added scopes for filtering Listings and Purchases

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_one :purchase 
   enum status: { available_listing: 0, pending_completion: 1, completed_listing: 2 }
+  scope :filter_by_status, -> (status) { where status: status }
 end
