acdc1ba3eaf84fda7cbf969cdbdf4344e82ae5b9
HughG-50 <hugh.greethead@gmail.com>
2020-05-20 16:43:14 +1000

    Updated edit profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,86 +1,84 @@
 User.destroy_all()
 
 # User seeds
-# case Rails.env
-# when "development"
-    user = User.new
-    user.email = "admin@admin.com"
-    user.password = "admin123"
-    user.username = "admin"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "1FfmbHfnpaZjKFvyi1okTjJJusN455paPH"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Sydney"
-    user.save!
+user = User.new
+user.email = "admin@admin.com"
+user.password = "admin123"
+user.username = "admin"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "1FfmbHfnpaZjKFvyi1okTjJJusN455paPH"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Sydney"
+user.save!
 
-    user = User.new
-    user.email = "alice@admin.com"
-    user.password = "admin123"
-    user.username = "Alice"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "1LWPSnSdN8yYzVnfkYA6wpqZwQyVu4UDNY"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Melbourne"
-    user.save!
+user = User.new
+user.email = "alice@admin.com"
+user.password = "admin123"
+user.username = "Alice"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "1LWPSnSdN8yYzVnfkYA6wpqZwQyVu4UDNY"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Melbourne"
+user.save!
 
-    user = User.new
-    user.email = "bob@admin.com"
-    user.password = "admin123"
-    user.username = "Bob"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "19yQtcX8JPkSTNyvA7sB2AQYAhmgt5t42c"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Melbourne"
-    user.save!
+user = User.new
+user.email = "bob@admin.com"
+user.password = "admin123"
+user.username = "Bob"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "19yQtcX8JPkSTNyvA7sB2AQYAhmgt5t42c"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Melbourne"
+user.save!
 
-    user = User.new
-    user.email = "hugh@admin.com"
-    user.password = "admin123"
-    user.username = "Hugh"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "1mn9kFzaspNCpnwKHQVeRkQK3ZsuWi8Jm"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Sydney"
-    user.save!
+user = User.new
+user.email = "hugh@admin.com"
+user.password = "admin123"
+user.username = "Hugh"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "1mn9kFzaspNCpnwKHQVeRkQK3ZsuWi8Jm"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Sydney"
+user.save!
 
-    user = User.new
-    user.email = "morgan@admin.com"
-    user.password = "admin123"
-    user.username = "Morgan"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "1EtdZKknyXUtzRqLa4ZLJn5LPkRphqhZDe"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Sydney"
-    user.save!
+user = User.new
+user.email = "morgan@admin.com"
+user.password = "admin123"
+user.username = "Morgan"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "1EtdZKknyXUtzRqLa4ZLJn5LPkRphqhZDe"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Sydney"
+user.save!
 
-    user = User.new
-    user.email = "ross@admin.com"
-    user.password = "admin123"
-    user.username = "Ross"
-    user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    user.btc_address = "12hw4mhjEVBEjkfruJJC1Tr42whn1eZZ8D"
-    user.feedback_score = nil
-    user.num_btc_purchases = 0
-    user.num_completed_listings = 0
-    user.country = "Australia"
-    user.city = "Sydney"
-    user.save!
-# end
+user = User.new
+user.email = "ross@admin.com"
+user.password = "admin123"
+user.username = "Ross"
+user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+user.btc_address = "12hw4mhjEVBEjkfruJJC1Tr42whn1eZZ8D"
+user.feedback_score = nil
+user.num_btc_purchases = 0
+user.num_completed_listings = 0
+user.country = "Australia"
+user.city = "Sydney"
+user.save!
+
 
 # Seeding structure - user actions
 # Admin, Alice and Bob will create Listings only, will not have any purchases
